bbbab190268d99c55d5453c10d79b3fcff317ebd
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-29 13:39:11 +0530

    Added LinkedListUC1

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -3,6 +3,7 @@ package com.bridgelabz.linkedlist;
 import java.util.LinkedList;
 
 public class LinkedListMain {
+
     public static void main(String[] args) {
 
         LinkedList<Integer> linkedList1 = new LinkedList<>();
